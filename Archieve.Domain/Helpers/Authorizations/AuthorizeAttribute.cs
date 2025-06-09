97d2637798303e8675024a093c3455722c5ea487
aaroke <arokeaugustine@gmail.com>
2025-06-09 22:21:26 +0100

    fixed permission attribute

---
+++ Archieve.Domain/Helpers/Authorizations/AuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Archieve.Core.Contracts.TransferObjects.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Archieve.Domain.Helpers.Authorizations
+{
+    [AttributeUsage( AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (Users)context.HttpContext.Items["User"];
+            if(user == null)
+            {
+                context.Result = new JsonResult(new {message = "Unauthorized"})
+                { StatusCode = StatusCodes.Status401Unauthorized};
+            }
+        }
+    }
+}
--- Archieve.Domain/Helpers/Authorizations/HasPermissionAttribute.cs
+++ Archieve.Domain/Helpers/Authorizations/HasPermissionAttribute.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Archieve.Core.Contracts.Enums;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Archieve.Domain.Helpers.Authorizations
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
-    public class HasPermissionAttribute : AuthorizeAttribute
+    public class HasPermissionAttribute : Microsoft.AspNetCore.Authorization.AuthorizeAttribute
     {
-        public HasPermissionAttribute(Permissions permissions) 
-            : base(permissions.ToString()){ }
+        public HasPermissionAttribute(Permissions permissions): 
+            base(permissions.ToString()){ }
     }
 }
